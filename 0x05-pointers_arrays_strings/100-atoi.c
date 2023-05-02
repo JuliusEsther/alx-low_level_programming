706af86ff5603f01a52d0d459f139c9cb562733f
JuliusEsther <udohnsekesther@gmail.com>
2023-05-02 11:11:31 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,6 @@
  */
 
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
@@ -20,7 +19,6 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-
 		len++;
 
 	while (i < len && f == 0)
@@ -28,23 +26,19 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 	if (s[i] >= '0' && s[i] <= '9')
-
 	{
 		digit = s[i] - '0';
 		if (d % 2)
 			digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-
+		n = n * 10 + digit;
+		f = 1;
 		if (s[i + 1] < '0' || s[i + 1] > '9')
-
-				break;
+			break;
 		f = 0;
 	}
 		i++;
 	}
 	if (f == 0)
-
 		return (0);
 
 	return (n);
