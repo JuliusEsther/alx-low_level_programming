1dfb97e542ae33f825efd88c2625ff5a77df04ea
JuliusEsther <udohnsekesther@gmail.com>
2023-05-02 05:19:06 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
-* _puts - prints a string, followed by a new line, to stdout
-* @str: string to print
+*_puts - prints a string, followed by a new line, to stdout
+*@str: string to print
 */
 
-void_puts(char *str)
+int void_puts(char *str)
 
 {
 
