bd56fa54fbfa131a3c0a4fb593655fbc5e474c55
JuliusEsther <udohnsekesther@gmail.com>
2023-05-21 09:07:29 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- *_memset - fills memory with a constant byte
- *@s: memory area to be filled
- *@b: char to copy
- *@n: number of times to copy b
- *
- *Return: pointer to the memory area s
- */
+*_memset - fills memory with a constant byte
+*@s:memory area to be filled
+*@b:char to copy
+*@n:number of times to copy b
+*
+*Return: pointer to the memory area s
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -18,6 +18,7 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+
 
 	return (s);
 }
