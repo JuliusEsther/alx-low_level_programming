ee3fe6d63c5bcf30f4526eb5aa4ff215bd820290
JuliusEsther <udohnsekesther@gmail.com>
2023-05-21 09:01:31 -0700

    task 2 updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,14 @@
  *Return: pointer to the memory area s
  */
 
-char *_memset(char *s, char b, unsigned int p)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < p; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
 
-	
 	return (s);
 }
